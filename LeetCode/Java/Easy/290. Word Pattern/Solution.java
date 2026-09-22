8ea0ed55a299f2aa8c2efe7f931ae6a25325ba1e
Yaasri <yaasrik11@gmail.com>
2026-09-22 17:24:02 +0530

    Add Easy Java Solution for 290. Word Pattern from LeetCode

---
+++ LeetCode/Java/Easy/290. Word Pattern/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        HashMap<Character, String> map = new HashMap<>();
+
+        if(words.length != pattern.length()) {
+            return false;
+        }
+
+        for(int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+
+            if(map.containsKey(ch)) {
+                if(map.get(ch).equals(words[i])) {
+                    continue;
+                }
+
+                if(!map.get(ch).equals(words[i])) {
+                    return false;
+                }
+            }
+
+            if(map.containsValue(words[i])) {
+                return false;
+            }
+
+            map.put(ch, words[i]);
+        }
+
+        return true;
+    }
+}
